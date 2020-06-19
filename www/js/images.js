b50541caaf22361f2a2a83948943980cd992265c
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-06-19 15:38:35 -0300

    lightbox

--- www/js/images.js
+++ www/js/images.js
@@ -3,6 +3,14 @@ $(() => {
     $("#my-image-input").click();
   });
 
+  $("#my-lightbox-image").click((event) => {
+    event.stopPropagation();
+  });
+
+  $("#my-lightbox-container").click(() => {
+    $("#my-lightbox-container").css("display", "none");
+  });
+
   $("#my-image-input").change((event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
@@ -44,7 +52,22 @@ $(() => {
 socket.on('image', addImage);
 
 function addImage(image) {
-  const mStyle = `style="background-image: url(${image.data64});"`;
-  $("#my-image-wall").append(`<div ${mStyle} class="image-common image-${image.orientation}"></div>`);
+  const mStyle = `background-image: url(${image.data64});`;
+
+  const imgEl = document.createElement('div');
+  imgEl.classList.add('image-common');
+  imgEl.classList.add(`image-${image.orientation}`);
+  imgEl.setAttribute('style', mStyle);
+
+  imgEl.addEventListener('click', (event) => {
+    openLightbox(event.target.style.backgroundImage);
+  });
+
+  $("#my-image-wall").append(imgEl);
   $("#my-image-wall-overflow").scrollTop($("#my-image-wall").prop("scrollHeight"));
+}
+
+function openLightbox(img) {
+  $("#my-lightbox-image").css('backgroundImage', img);
+  $("#my-lightbox-container").css("display", "block");
 }
