2063855a9385c942ec1a2a740c46a0b59cda4188
Thiago Hersan <thiago.hersan+github@gmail.com>
2020-06-19 22:49:14 -0300

    rainbow text

--- www/js/poster.js
+++ www/js/poster.js
@@ -3,19 +3,25 @@ const colors = [
   '#ffcd00',
   '#008000',
   '#007dc6',
-  '#c80477'
+  '#c80477',
+  'linear-gradient(45deg, red, orange, yellow, green, blue, violet)'
 ];
 
 let currentColor = colors[0];
+let rainbow;
+
+function preload() {
+  rainbow = loadImage('../imgs/rainbow.jpg');
+}
 
 function createColorButtons() {
   for(let i = 0; i < colors.length; i++) {
     const colorEl = document.createElement('div');
     colorEl.classList.add('poster-color-common');
-    colorEl.style.backgroundColor = colors[i];
+    colorEl.style.background = colors[i];
 
     colorEl.addEventListener('click', (event) => {
-      currentColor = event.target.style.backgroundColor;
+      currentColor = event.target.style.background;
       renderTextP5();
     });
     $("#my-poster-colors-container").append(colorEl);
@@ -41,13 +47,23 @@ function renderTextP5() {
   const mMessage = $("#my-poster-input").val().toUpperCase();
 
   background(0,0,0);
-  fill(currentColor);
-  stroke(currentColor);
+  console.log(currentColor);
+  if(currentColor === colors[colors.length - 1]) {
+    fill(255);
+    stroke(255);
+  } else {
+    fill(currentColor);
+    stroke(currentColor);
+  }
 
   textSize(128);
   textLeading(130);
   textAlign(CENTER, CENTER);
   text(mMessage, padding, padding, width - 2 * padding, height - 2 * padding);
+  
+  if(currentColor === colors[colors.length - 1]) {
+    blend(rainbow, 0, 0, rainbow.width, rainbow.height, 0, 0, width, height, DARKEST);
+  }
 }
 
 $(() => {
