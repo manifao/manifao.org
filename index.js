ac275315ac77d9a3c6578170c0a38a3e804ab3b8
andrehcorrea <33695852+andrehcorrea@users.noreply.github.com>
2020-07-31 14:44:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use(express.json({ limit: '5mb' }));
 app.use(helmet());
 
 app.get('/', function(req, res) {
-  const startTime = moment.tz('2020-08-02T17:55:00', 'America/Sao_Paulo').format('x');
-  const endTime = moment.tz('2020-08-02T20:00:00', 'America/Sao_Paulo').format('x');
+  const startTime = moment.tz('2020-08-02T17:50:00', 'America/Sao_Paulo').format('x');
+  const endTime = moment.tz('2020-08-02T21:00:00', 'America/Sao_Paulo').format('x');
   const now = moment().tz('America/Sao_Paulo').format('x');
 
   if(now < startTime) {
