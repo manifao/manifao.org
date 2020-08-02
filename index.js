b29fc593fd7774ad793cb79aa78d987aaddccd8d
andrehcorrea <33695852+andrehcorrea@users.noreply.github.com>
2020-08-02 20:47:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(helmet());
 
 app.get('/', function(req, res) {
   const startTime = moment.tz('2020-08-02T17:50:00', 'America/Sao_Paulo').format('x');
-  const endTime = moment.tz('2020-08-02T21:00:00', 'America/Sao_Paulo').format('x');
+  const endTime = moment.tz('2020-08-02T21:30:00', 'America/Sao_Paulo').format('x');
   const now = moment().tz('America/Sao_Paulo').format('x');
 
   if(now < startTime) {
