55745101ecb6110e754e3d47a32d84e05f095bd1
yannickfalisse <70851276+yannickfalisse@users.noreply.github.com>
2020-10-22 20:36:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(helmet());
 
 app.get('/', function(req, res) {
   const startTime = moment.tz('2020-10-22T20:00:00', 'America/Sao_Paulo').format('x');
-  const endTime = moment.tz('2020-10-22T21:00:00', 'America/Sao_Paulo').format('x');
+  const endTime = moment.tz('2020-10-22T20:40:00', 'America/Sao_Paulo').format('x');
   const now = moment().tz('America/Sao_Paulo').format('x');
 
   if(now < startTime) {
